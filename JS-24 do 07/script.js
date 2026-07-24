9b0c2f5a6ae5527e6cfaca0c0a5e91af7a928ef9
jfgsilva2010-creator <jfgsilva2010@gmail.com>
2026-07-24 13:03:56 -0300

    Add files via upload

---
+++ JS-24 do 07/script.js
@@ -0,0 +1,14 @@
+// Solicita o nome do usuário
+let nome = prompt("Qual é o seu nome?");
+
+// Solicita a cidade do usuário
+let cidade = prompt("De qual cidade você é?");
+
+// Exibe uma mensagem de boas-vindas
+alert("Seja bem-vindo(a), " + nome + "!");
+
+// Mostra o nome e a cidade no console
+console.log("Nome: " + nome);
+console.log("Cidade: " + cidade);
+
+
