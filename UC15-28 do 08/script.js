727da59114ce82380977f6f0da0aacb916f767e6
jfgsilva2010-creator <jfgsilva2010@gmail.com>
2026-08-28 12:11:30 -0300

    Add files via upload

--- UC15-28 do 08/script.js
+++ UC15-28 do 08/script.js
@@ -1,4 +1,3 @@
-// Questão 1 - Operações matemáticas
 
 let numero1 = 10;
 let numero2 = 5;
@@ -9,43 +8,39 @@ console.log("Multiplicaçao:", numero1 * numero2);
 console.log("Divisao:", numero1 / numero2);
 
 
-// Questão 2 - Dobro e triplo
 
 let numero = 8;
 
-console.log("Dobro:", numero * 2);
+console.log("Dobro:", numero * 2);1
 console.log("Triplo:", numero * 3);
 
 
-// Questão 3 - Nome e idade do usuário
 
-let nome = prompt("Digite seu nome:");
-let idade = prompt("Digite sua idade:");
+let nome = prompt("Neymar jr:");
+let idade = prompt("18 anos:");
 
-console.log("Ola, " + nome + "! Seja bem-vindo! Você tem " + idade + " anos.");
+console.log("Ola, " + nome + "! Seja bem-vindo! Voce tem " + idade + " anos.");
 
 
-// Questão 4 - Média de três notas
 
-let nota1 = Number(prompt("Digite a primeira nota:"));
-let nota2 = Number(prompt("Digite a segunda nota:"));
-let nota3 = Number(prompt("Digite a terceira nota:"));
+let nota1 = Number(prompt("10:"));
+let nota2 = Number(prompt("07:"));
+let nota3 = Number(prompt("08:"));
 
 let media = (nota1 + nota2 + nota3) / 3;
 
 console.log("Media:", media);
 
 
-// Questão 5 - Sistema de login
 
 let usuarioCorreto = "admin";
 let senhaCorreta = "1234";
 
-let usuario = prompt("Digite o usuário:");
-let senha = prompt("Digite a senha:");
+let usuario = prompt(" Neymar jr:");
+let senha = prompt("271210:");
 
 if (usuario !== usuarioCorreto) {
-    console.log("Usuário incorreto.");
+    console.log("Usuario incorreto.");
 } else if (senha !== senhaCorreta) {
     console.log("Senha incorreta.");
 } else {
@@ -53,10 +48,9 @@ if (usuario !== usuarioCorreto) {
 }
 
 
-// Questão 6 - Número maior
 
-let primeiroNumero = Number(prompt("Digite o primeiro número:"));
-let segundoNumero = Number(prompt("Digite o segundo número:"));
+let primeiroNumero = Number(prompt("22:"));
+let segundoNumero = Number(prompt("7:"));
 
 if (primeiroNumero > segundoNumero) {
     console.log("O maior numero é " + primeiroNumero);
