2b4867bc6e8f848b9d626a77f1675fa3c686cf33
jfgsilva2010-creator <jfgsilva2010@gmail.com>
2026-09-17 10:25:11 -0300

    Add files via upload

---
+++ UC 15 DOM 17 do 9/script.js
@@ -0,0 +1,8 @@
+function mudarTexto() {
+    document.getElementById
+    ("titulo").textContent = "voce clicou no botão!!" ; 
+}
+
+function mudarCor() {
+    document.getElementById("mensagem").style.color = "blue";
+}
